e2e08974ca84f056d7365e0d69b3863e24a86367
bakhacode <bakhamitai@naver.com>
2024-01-10 20:42:38 +0900

    refactor: chapter14

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -68,7 +68,11 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 // ...
 
-export async function updateInvoice(id: string, formData: FormData) {
+export async function updateInvoice(
+  id: string,
+  prevState: State,
+  formData: FormData,
+) {
   const validatedFields = UpdateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
